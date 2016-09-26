bca930751b1529b1197e8cb538c7404cb6494110
zhongzhi107 <zhong.zhi@163.com>
2016-09-26 22:55:18 +0800

    init

---
+++ index.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+var util = require('util');
+var _ = require('lodash');
+var clearRequire = require('clear-require');
+require('jsmart');
+
+module.exports = function(options) {
+  options = _.assignIn({
+    encoding: 'utf-8',
+    extension: '.tpl',
+    templates: '.',
+    mockData: '.',
+    rewriteRules: {}
+  }, options);
+  return function(req, res, next) {
+    var urlObject = url.parse(req.url);
+    var pathname = options.rewriteRules[urlObject.pathname] || urlObject.pathname;
+    var templateAbsPath = path.resolve(path.join(options.templates, pathname));
+    var dataAbsPath = path.resolve(path.join(options.mockData, pathname.replace(options.extension, '.js')));
+    if (fs.existsSync(templateAbsPath)) {
+      var tpl = fs.readFileSync(templateAbsPath, {encoding: options.encoding});
+      var context = {};
+      if (fs.existsSync(dataAbsPath)) {
+        try {
+          var contextExport = require(dataAbsPath);
+          if (util.isFunction(contextExport)) {
+            context = contextExport(req, res);
+          } else {
+            context = contextExport;
+          }
+        }
+        catch (e) {
+          console.log('File "' + dataAbsPath + ' require failed.\n' + e);
+        }
+      }
+      var compiledTpl = new jSmart(tpl);
+      var output = compiledTpl.fetch(context);
+      res.end(output);
+    } else {
+      next();
+    }
+  };
+};
