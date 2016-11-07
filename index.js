0f6a2f654a77f911db65110894940e74a1ea1d99
zhongzhi107 <zhong.zhi@163.com>
2016-11-07 22:59:54 +0800

    v1.0.4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var url = require('url');
 var util = require('util');
-var assign = require('object-assign');
+var assign = require('object-assign-deep');
 var clearRequire = require('clear-require');
 var Handlebars = require('handlebars');
 
@@ -11,6 +11,7 @@ module.exports = function(options) {
     encoding: 'utf-8',
     extension: '.hbs',
     templates: '.',
+    globalData: '__global.js',
     mockData: '.',
     rewriteRules: {}
   }, options);
@@ -19,25 +20,36 @@ module.exports = function(options) {
     var pathname = options.rewriteRules[urlObject.pathname] || urlObject.pathname;
     var templateAbsPath = path.resolve(path.join(options.templates, pathname));
     var dataAbsPath = path.resolve(path.join(options.mockData, pathname.replace(options.extension, '.js')));
+    var globalDataPath = path.resolve(path.join(options.mockData, options.globalData));
     if (fs.existsSync(templateAbsPath)) {
       var tpl = fs.readFileSync(templateAbsPath, {encoding: options.encoding});
-      var context = {};
-      if (fs.existsSync(dataAbsPath)) {
-        try {
-          var contextExport = require(dataAbsPath);
-          if (util.isFunction(contextExport)) {
-            context = contextExport(req, res);
-          } else {
-            context = contextExport;
-          }
-        }
-        catch (e) {
-          console.log('File "' + dataAbsPath + ' require failed.\n' + e);
+      if (fs.existsSync(templateAbsPath)) {
+      var globalContext = {};
+      if (fs.existsSync(globalDataPath)) {
+        var gcontext = require(globalDataPath);
+        if (util.isFunction(gcontext)) {
+          globalContext = gcontext(req, res);
+        } else {
+          globalContext = gcontext;
         }
       }
-      var compiledTpl = Handlebars.compile(tpl);
-      var output = compiledTpl(context);
-      res.end(output);
+      var pageContext = {};
+      if (fs.existsSync(dataAbsPath)) {
+        var pcontext = require(dataAbsPath);
+        if (util.isFunction(pcontext)) {
+          pageContext = pcontext(req, res);
+        } else {
+          pageContext = pcontext;
+        }
+      }
+      try {
+        var compiledTpl = Handlebars.compile(tpl);
+        var output = compiledTpl(assign(globalContext, pageContext));
+        res.end(output);
+      } catch (e) {
+        console.log(e);
+        next();
+      }
     } else {
       next();
     }
